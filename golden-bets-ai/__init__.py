a926a459244f57acc89982ed0e7da45b406ac12f
dannythehat <dannythehat2@gmail.com>
2025-11-15 03:45:45 +0200

    Add Golden Bets AI module init

---
+++ golden-bets-ai/__init__.py
@@ -0,0 +1,7 @@
+"""
+Golden Bets AI Module
+Filters Smart Bets predictions to identify 1-3 daily picks with 85%+ confidence
+"""
+from .filter import GoldenBetsFilter
+
+__all__ = ['GoldenBetsFilter']
